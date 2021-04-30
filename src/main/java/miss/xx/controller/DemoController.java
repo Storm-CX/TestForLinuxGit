f3dbf19026aed2a48c963fe1d86f593cc9f52624
陈翔 <xiang3.chen@ly.com>
2021-04-30 13:22:21 +0800

    first commit

---
+++ src/main/java/miss/xx/controller/DemoController.java
@@ -0,0 +1,13 @@
+package miss.xx.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
